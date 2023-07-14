de5cb98b64255cbab839cfbe1c1c6e97b9777767
AKHILESHPRATAP96 <114492226+AKHILESHPRATAP96@users.noreply.github.com>
2023-07-15 03:39:18 +0530

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/app')
 const mongoose = require('mongoose')
 const port = 3000
 
-// Parse JSON bodies (as sent by API clients)
+//Parse JSON bodies that API clients send.
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 

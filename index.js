09a2cafd24acf0ba867bfc3689e2f4502950d18d
AKHILESHPRATAP96 <114492226+AKHILESHPRATAP96@users.noreply.github.com>
2023-07-16 03:46:51 +0530

    minor fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = require('./src/app')
 const mongoose = require('mongoose')
-const port = 3000
+const port = process.env.PORT || 3000
 
 //Parse JSON bodies that API clients send.
 app.use(express.json())
